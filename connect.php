62d8a8d456adb61292e87d33d0edac6258d66c46
David Nanjila <nanjiladavid2@gmail.com>
2024-07-25 14:50:28 +0300

    rf: update the connect.php page not display connection succesful when we connect to the the db successfully
    Signed-off-by: David Nanjila <nanjiladavid2@gmail.com>

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 $host = 'localhost';
-$dbname = 'learning_management';
+$dbname = 'school';
 $username ='root';
 $password = '1234218@Nanjila22';
 
@@ -8,6 +8,4 @@ $conn =  new mysqli($host, $username, $password, $dbname);
 
 if($conn->connect_error){
     die('Database connection error: ' . $conn->connect_error);
-}else{
-    echo 'Database connection successful';
 }
